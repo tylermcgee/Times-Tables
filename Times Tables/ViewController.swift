4d491cc991d27ec6e9065e55f4277d00b5d0aa74
Tyler McGee <tmcgee2481@gmail.com>
2017-07-05 10:23:48 -0400

    Committed 7/05/17.

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        
+        table.reloadData()
+        
     }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            return 50
+        
+        }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+            
+            cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+            
+            return cell
+            
+            
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    
+    
 
-
-}
 
